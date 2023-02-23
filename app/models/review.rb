845a8e68f31cbc8abfb5db0bc8690553267af51e
+ <+>
2023-02-23 15:59:00 +0300

    completed code along

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,10 @@
 class Review < ActiveRecord::Base
-  
+    # First method
+    # def game
+    #     # self is the review instance
+    #     Game.find(self.game_id)
+    # end
+
+    # Second method
+    belongs_to :game
 end
--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,28 @@ require 'bundler/setup'
 Bundler.require(:default, ENV["RACK_ENV"])
 
 require_all 'app/models'
+
+# Working with associations
+
+# Using the belongs_to association
+# Get a review instance
+# review = Review.first
+# call the #game instance method to return a Game instance
+# review.game
+# => #<Game:0x00007f9de1710be8 id: 1, title: "Metroid Prime",...>
+
+# Using the has_many :reviews macro in our Game class generates an instance method,
+# #reviews that we can use to access the data about reviews from the game:
+
+# Get a game instance
+# game = Game.first
+# call the #reviews instance method to return a list of Review instances
+# game.reviews
+# => [#<Review:0x00007f9ddcb09100 id: 1, score: 6, ...>, #<Review:0x00007f9ddcb08f98 id: 2,
+
+# Create a review
+# review = Review.create(score: 8, comment: "wow, what a game")
+# Create a game associated with the review
+# review.create_game(title: "My favorite game")
+# Save the association
+# review.save
